538b9f3f9178714af2e75eb65718d436348995da
JunHyung1206 <pjh120610@naver.com>
2025-10-15 04:55:31 +0900

    refactor(CustomSeparator) : separator 추출 로직 변경
    연산자 부분을 먼저 추출하는 방식으로 변경
    1. 커스텀 연산자가 있는 부분을 추출
    2. 커스텀 연산자가 있다면 해당 수식에서 커스텀 연산자가 있는 부분을 제거
    3. 마커를 제거한 후 해당 커스텀 연산자를 연산자 목록에 추가
    4. 합할 피연산자만 추출하여 반환

--- src/main/java/calculator/separator/SeparatorProcessor.java
+++ src/main/java/calculator/separator/SeparatorProcessor.java
@@ -6,41 +6,53 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SeparatorProcessor {
+    List<String> separators = new ArrayList<>();
+
+    // TODO: 기본 구분자, 커스텀 구분자 어떻게 처리하면 좋을지에 대해 고민한다.
+    public SeparatorProcessor() {
+        separators.add(",");
+        separators.add(":");
+    }
+
     public List<Integer> processingSeparator(String expression) {
-        String customSeparator = extractCustomSeparator(expression);
+        // 1. 커스텀 연산자가 있는 부분을 추출합니다.
+        String customSeparatorSection = extractCustomSeparatorSection(expression);
 
-        if (customSeparator != null) {
-            expression = expression.replace(CustomSeparatorMarker.startSep + customSeparator + CustomSeparatorMarker.endSep, "");
+        if (customSeparatorSection != null) {
+            // 2. 커스텀 연산자가 있다면 해당 수식에서 커스텀 연산자가 있는 부분을 제거합니다.
+            expression = removeCustomSeparatorSection(expression, customSeparatorSection);
+
+            // 3. 마커를 제거한 후 해당 커스텀 연산자를 연산자 목록에 추가합니다.
+            separators.add(removeMarker(customSeparatorSection));
         }
-
-        return extractOperand(expression, customSeparator);
+        // 4. 합할 피연산자만 추출하여 반환합니다.
+        return extractOperand(expression, separators);
     }
 
-    private String separatePostProcessing(String preProcessedExpression){
-        return preProcessedExpression.substring(CustomSeparatorMarker.startSep.length(), preProcessedExpression.length() - CustomSeparatorMarker.endSep.length());
+    private String removeCustomSeparatorSection(String expression, String customSeparatorSection) {
+        return expression.replace(customSeparatorSection, "").strip();
     }
 
-    public String extractCustomSeparator(String expression){
-        String separation = null;
+    public String extractCustomSeparatorSection(String expression){
         Pattern separatorPattern = Pattern.compile(Pattern.quote(CustomSeparatorMarker.startSep)  + "." + Pattern.quote(CustomSeparatorMarker.endSep));
-
         Matcher matcher = separatorPattern.matcher(expression);
         if (matcher.find()) {
-            separation = separatePostProcessing(matcher.group());
+            return matcher.group();
         }
-
-        return separation;
+        return null;
     }
 
-    public List<Integer> extractOperand(String expression, String customSeparator){
-        List<Integer> operands = new ArrayList<>();
-        expression = expression.replace(":", " ").replace(",", " ");
-        if (customSeparator != null){
-            expression = expression.replace(customSeparator, " ");
-        }
+    private String removeMarker(String customSeparatorSection){
+        return customSeparatorSection.replace(CustomSeparatorMarker.startSep, "").replace(CustomSeparatorMarker.endSep, "");
+    }
 
-        for (String s : expression.split(" ")) {
-            operands.add(Integer.parseInt(s));
+    public List<Integer> extractOperand(String expression, List<String> separators){
+        List<Integer> operands = new ArrayList<>();
+        for (String separator : separators) {
+            expression = expression.replace(separator, " ");
+        }
+        for (String operand : expression.split(" ")) {
+            operands.add(Integer.parseInt(operand));
         }
         return operands;
     }
