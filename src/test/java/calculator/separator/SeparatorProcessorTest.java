ead235968b62548ad99e87fb534167e805444b24
JunHyung1206 <pjh120610@naver.com>
2025-10-15 15:26:25 +0900

    test : 어플리케이션 테스트 수정
    1. Calculator Test는 전체 로직 전반에 걸쳐서 확인하도록 수정
    2. 커스텀 연산자만 있는 경우, 공백 처리에 대한 오류 테스트 추가

--- src/test/java/calculator/separator/SeparatorProcessorTest.java
+++ src/test/java/calculator/separator/SeparatorProcessorTest.java
@@ -1,22 +1,24 @@
 package calculator.separator;
 
-import calculator.Calculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeparatorProcessorTest {
+    SeparatorProcessor separatorProcessor;
+
+    @BeforeEach
+    void setUp(){
+        separatorProcessor = new SeparatorProcessor();
+    }
+
     @Test()
     @DisplayName("커스텀 구분자 부분을 추출한다")
     void extractCustomSeparatorSectionTest() {
-        SeparatorProcessor separatorProcessor = new SeparatorProcessor();
-
         String expression = "//;\\n1;2;3";
         String result = separatorProcessor.extractCustomSeparatorSection(expression);
 
@@ -25,13 +27,21 @@ class SeparatorProcessorTest {
 
     @Test
     @DisplayName("지정된 구분자를 통해 숫자만 추출한다.")
-    void NoCustomSeparationTest(){
-        SeparatorProcessor separatorProcessor = new SeparatorProcessor();
+    void separationTest(){
         List<Integer> operands = separatorProcessor.extractOperand("1,2:3", List.of(":",",",";"));
 
         List<Integer> expect = List.of(1, 2, 3);
         assertThat(operands).isEqualTo(expect);
 
         assertThrows(NumberFormatException.class, () -> separatorProcessor.extractOperand("1,2;3:4,5", List.of(":",",")));
+    }
+
+    @Test
+    @DisplayName("빈 문자열이면 빈 리스트를 반환한다.")
+    void blankTest(){
+        List<Integer> result = separatorProcessor.extractOperand("", List.of(":", ","));
+
+        assertThat(result).isEqualTo(List.of());
+
     }
 }
