20a335697d4ff8ca82d9a0622d81d5cf15bbcac4
JunHyung1206 <pjh120610@naver.com>
2025-10-15 15:26:25 +0900

    test : 어플리케이션 테스트 수정
    1. Calculator Test는 전체 로직 전반에 걸쳐서 확인하도록 수정
    2. 커스텀 연산자만 있는 경우, 공백 처리에 대한 오류 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,4 +10,61 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class CalculatorTest {
+
+    Calculator calculator;
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("성공 케이스 : 기본 구분자만 있는 경우")
+    void successCaseNoCustomSeparation(){
+        String expression;
+        int result;
+
+        expression = "2:2";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(4);
+
+        expression = "1,2:3:4";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(10);
+
+        expression = "1";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    @DisplayName("성공 케이스 : 커스텀 구분자도 같이 존재하는 경우")
+    void successCaseWithCustomSeparation(){
+        String expression;
+        int result;
+
+        expression = "//;\\n2;2";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(4);
+
+        expression = "//t\\n1,2:3:4t5t6";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(21);
+    }
+
+    @Test
+    @DisplayName("입력이 없는 경우")
+    void notInput(){
+        String expression;
+        int result;
+
+        expression = "//;\\n";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(0);
+
+        expression = "";
+        result = calculator.processCalculate(expression);
+        assertThat(result).isEqualTo(0);
+    }
+
 }
