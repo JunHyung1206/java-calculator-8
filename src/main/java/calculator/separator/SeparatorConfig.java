ef858059c7380c0a82adac1a751c9e8e6ab481ad
JunHyung1206 <pjh120610@naver.com>
2025-10-17 03:24:31 +0900

    fix(Separator) : 기본 구분자 오류
    ; -> : 으로 변경

--- src/main/java/calculator/separator/SeparatorConfig.java
+++ src/main/java/calculator/separator/SeparatorConfig.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 /**기본 구분자 정보 관리 */
 public final class SeparatorConfig {
-    public static final List<String> DEFAULT_SEPARATORS = List.of(",", ";");
+    public static final List<String> DEFAULT_SEPARATORS = List.of(",", ":");
 }
