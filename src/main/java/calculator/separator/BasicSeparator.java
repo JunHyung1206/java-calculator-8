068f0af73443cdfb2c8aceceeff6a8b89e49c5c2
JunHyung1206 <pjh120610@naver.com>
2025-10-16 16:12:30 +0900

    refactor(BasicSeparator) : extractOperand 로직 변경
    1. 빈칸인 경우는 바로 빈 리스트를 반환하도록 수정
    2. split으로 바로 진행

--- src/main/java/calculator/separator/BasicSeparator.java
+++ src/main/java/calculator/separator/BasicSeparator.java
@@ -8,20 +8,17 @@ public class BasicSeparator implements Separator {
         List<Integer> operands = new ArrayList<>();
         String splitPattern = "[" + String.join("", basicSeparators) + "]";
 
-        String removedSeparators = expression.replaceAll(splitPattern, " ");
-        if(removedSeparators.isBlank()){
-            return operands;
-        }
-
-        for (String s : removedSeparators.split(" ")) {
+        for (String s : expression.split(splitPattern)) {
             operands.add(Integer.parseInt(s));
         }
         return operands;
     }
 
-
     @Override
     public List<Integer> split(String expression) {
+        if (expression.isBlank()){
+            return new ArrayList<>();
+        }
         return extractOperand(expression);
     }
 }
