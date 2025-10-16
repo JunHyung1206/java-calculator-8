239019813bd4d90810ae1ca7ecc722a86feb74ce
JunHyung1206 <pjh120610@naver.com>
2025-10-16 21:57:02 +0900

    fix(validator) : 커스텀 구분자는 한개로만 제한한다.

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Validator {
     public static boolean validate(String input){
         String matchResult;
-        Pattern validPattern = Pattern.compile("(" + CustomSeparator.getCustomRegex() + ")" + "*" + "(\\d+([^\\s\\w]\\d+)*|)");
+        Pattern validPattern = Pattern.compile("(" + CustomSeparator.getCustomRegex() + ")" + "?" + "(\\d+([^\\s\\w]\\d+)*|)");
         Matcher matcher = validPattern.matcher(input);
 
         if(matcher.find()){
