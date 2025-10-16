3244cf04e94dd3d2236df246da9a9cdb6fd6d514
JunHyung1206 <pjh120610@naver.com>
2025-10-16 21:39:20 +0900

    style : 주석 제거

--- src/main/java/calculator/separator/SeparatorProcessor.java
+++ src/main/java/calculator/separator/SeparatorProcessor.java
@@ -10,11 +10,10 @@ public class SeparatorProcessor {
     }
 
     public List<Integer> extractOperand(String expression) {
-
-        // TODO: 분리하는 부분은 커스텀 연산자가 담당한다.
         // 1. 커스텀 연산자 부분이 있는지 확인한다.
         // 2. 있다면 separator를 CustomSeparator로 사용한다. 없다면 separator를 BasicSeparator로 사용한다.
         // 3. 분리해서 반환한다.
+
         if(CustomSeparator.isExistCustomSeparator(expression)){
             separators = new CustomSeparator();
         }
--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -7,9 +7,7 @@ import java.util.regex.Pattern;
 
 public class Validator {
     public static boolean validate(String input){
-
         String matchResult;
-        // 원하는 것 : "(//.\\n)*(|\\d+)"
         Pattern validPattern = Pattern.compile("(" + CustomSeparator.getCustomRegex() + ")" + "*" + "(\\d+([^\\s\\w]\\d+)*|)");
         Matcher matcher = validPattern.matcher(input);
 
